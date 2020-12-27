6ded98fcc69f2c989b54f9fbd25fee4640ae19bf
Imran2526 <imraninfo97@gmail.com>
2020-12-27 15:42:46 +0530

    [Imran] Add: Insert Record in Database

---
+++ InsertRecord.sql
@@ -0,0 +1,10 @@
+insert into [dbo].[AddressBook] values
+
+('shaikh','imran','adarshanagar-pune','pune','maha',411015,'7890987654'),
+('nijam','sayyad','LAturCity','latur','maha',411012,'7890987445'),
+('maske','pratibha','mumbaiCity','mumbai','maha',411315,'7890987444'),
+('dandge','arti','punecolony','pune','maha',411015,'7890987111'),
+('shaikh','ajim','adarshanagar-pune','pune','maha',411015,'7890987666'),
+('shaikh','imran','adarshanagar-pune','pune','maha',411015,'7890098765')	
+
+select * from AddressBook
