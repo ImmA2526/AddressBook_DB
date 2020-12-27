00ba039295168b87a02f5569cb6318f602591cc1
Imran2526 <imraninfo97@gmail.com>
2020-12-27 15:41:20 +0530

    [Imran] Add: Create Table

---
+++ CreateTable.sql
@@ -0,0 +1,10 @@
+create table addressBook
+(
+	firstName varchar(20) not null,
+	lastName varchar(20) not null,
+	address varchar(60) not null,
+	city varchar(20) not null,
+	state varchar(20) not null,
+	zip varchar(6) not null,
+	phoneNumber varchar(10) not null
+)
